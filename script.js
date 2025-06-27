993dc02058cceeb2aa2020a45772444247ae2aea
yeshu0904 <yeshmettu0904@gmail.com>
2025-06-27 13:02:21 +0530

    static

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add any common JavaScript functionality here
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+    
+    // Any other common JS code
+});
